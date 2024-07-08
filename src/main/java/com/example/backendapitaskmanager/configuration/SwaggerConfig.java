29843a2503e979d6a3e7791c4b09875baccb5961
vyesman <viktor.yesman@ukr.net>
2024-07-08 10:04:39 +0300

    swagger config

---
+++ src/main/java/com/example/backendapitaskmanager/configuration/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.backendapitaskmanager.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicUserApi() {
+        return GroupedOpenApi.builder()
+                .group("Tasks")
+                .pathsToMatch("/api/taskmanager/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI();
+    }
+
+}
--- src/main/java/com/example/backendapitaskmanager/controller/TaskController.java
+++ src/main/java/com/example/backendapitaskmanager/controller/TaskController.java
@@ -6,6 +6,12 @@ import com.example.backendapitaskmanager.entity.enums.TaskStatus;
 import com.example.backendapitaskmanager.mapper.TaskMapper;
 import com.example.backendapitaskmanager.response.Response;
 import com.example.backendapitaskmanager.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +30,17 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Operation(summary = "Create task", tags = "task")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Create new task",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Response.class)))
+                    })
+    })
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public Response createTask(@Valid @RequestBody Task task) {
@@ -31,6 +48,17 @@ public class TaskController {
         return new Response("The task was successfully created with id = " + id);
     }
 
+    @Operation(summary = "Delete task", tags = "task")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Delete task from database",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Response.class)))
+                    })
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Response deleteTask(@PathVariable Long id) {
@@ -38,14 +66,36 @@ public class TaskController {
         return new Response("The task was successfully deleted");
     }
 
+    @Operation(summary = "Update task status", tags = "task")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Update task status",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Response.class)))
+                    })
+    })
     @PutMapping("/{id}/{status}")
     @ResponseStatus(HttpStatus.OK)
     public Response updateTaskStatus(@PathVariable Long id,
-                              @PathVariable TaskStatus status) {
+                                     @PathVariable TaskStatus status) {
         TaskStatus taskStatus = taskService.updateTaskStatus(id, status);
         return new Response("The task status was successfully changed to " + taskStatus);
     }
 
+    @Operation(summary = "Update task", tags = "task")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Update task fields",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Response.class)))
+                    })
+    })
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Response updateTask(@Valid @RequestBody Task task) {
@@ -53,6 +103,17 @@ public class TaskController {
         return new Response("The task fields was successfully changed");
     }
 
+    @Operation(summary = "Gets all tasks", tags = "task")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found the tasks",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = List.class)))
+                    })
+    })
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public List<TaskDto> getAllTasks() {
